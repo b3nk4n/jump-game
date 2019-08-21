17a2ac3e1ce1b121b562f9ffcac2d8c2a0d10565
Benjamin Sautermeister <benjamin.sautermeister@instana.com>
2019-08-21 15:56:00 +0200

    Use pooled effect for water splash

--- core/src/de/bsautermeister/jump/managers/WaterInteractionManager.java
+++ core/src/de/bsautermeister/jump/managers/WaterInteractionManager.java
@@ -2,6 +2,7 @@ package de.bsautermeister.jump.managers;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.ParticleEffect;
+import com.badlogic.gdx.graphics.g2d.ParticleEffectPool;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Rectangle;
@@ -13,15 +14,18 @@ import de.bsautermeister.jump.GameCallbacks;
 import de.bsautermeister.jump.assets.AssetPaths;
 
 public class WaterInteractionManager {
-    private ParticleEffect splashEffect = new ParticleEffect();
+    private ParticleEffectPool splashEffectPool;
+    private Array<ParticleEffectPool.PooledEffect> activeSplashEffects = new Array<ParticleEffectPool.PooledEffect>();
 
     private final GameCallbacks callbacks;
     private final Array<Rectangle> waterRegions;
     private final Array<Drownable> drownables = new Array<Drownable>();
 
     public WaterInteractionManager(TextureAtlas atlas, GameCallbacks callbacks, Array<Rectangle> waterRegions) {
+        ParticleEffect splashEffect = new ParticleEffect();
         splashEffect.load(Gdx.files.internal(AssetPaths.Pfx.SPLASH), atlas);
         splashEffect.scaleEffect(0.1f / Cfg.PPM);
+        splashEffectPool = new ParticleEffectPool(splashEffect, 8, 16);
 
         this.callbacks = callbacks;
         this.waterRegions = waterRegions;
@@ -33,6 +37,15 @@ public class WaterInteractionManager {
                 if (waterRegion.contains(drownable.getWorldCenter())) {
                     doDrown(drownable);
                 }
+            }
+        }
+
+        for (int i = activeSplashEffects.size - 1; i >= 0; i--) {
+            ParticleEffectPool.PooledEffect effect = activeSplashEffects.get(i);
+            effect.update(delta);
+            if (effect.isComplete()) {
+                activeSplashEffects.removeIndex(i);
+                effect.free();
             }
         }
     }
@@ -48,8 +61,9 @@ public class WaterInteractionManager {
     private void doDrown(Drownable drownable) {
         if (!drownable.isDead() && !drownable.isDrowning() && drownable.getLinearVelocity().y < -0.5f) {
             Vector2 center = drownable.getWorldCenter();
+            ParticleEffectPool.PooledEffect splashEffect = splashEffectPool.obtain();
             splashEffect.setPosition(center.x, center.y);
-            splashEffect.start();
+            activeSplashEffects.add(splashEffect);
             callbacks.touchedWater(drownable);
 
             drownable.drown();
@@ -57,6 +71,9 @@ public class WaterInteractionManager {
     }
 
     public void draw(SpriteBatch batch) {
-        splashEffect.draw(batch, Gdx.graphics.getDeltaTime());
+        for (int i = activeSplashEffects.size - 1; i >= 0; i--) {
+            ParticleEffectPool.PooledEffect effect = activeSplashEffects.get(i);
+            effect.draw(batch);
+        }
     }
 }
