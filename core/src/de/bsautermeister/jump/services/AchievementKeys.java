820136d07ded0def25dc89df10ff67deb2f654a1
Benjamin Sautermeister <benjamin.sautermeister@instana.com>
2020-08-11 21:12:03 +0200

    Use actual game services IDs / keys

--- core/src/de/bsautermeister/jump/services/AchievementKeys.java
+++ core/src/de/bsautermeister/jump/services/AchievementKeys.java
@@ -1,9 +1,9 @@
 package de.bsautermeister.jump.services;
 
 public interface AchievementKeys {
-    String BEERS_25 = "aaa";
-    String BEERS_50 = "bbb";
-    String BEERS_100 = "ccc";
-    String KILL_3 = "ddd";
-    String KILL_5 = "eee";
+    String BEERS_25 = "CgkIldPb1IEUEAIQAQ";
+    String BEERS_50 = "CgkIldPb1IEUEAIQAg";
+    String BEERS_100 = "CgkIldPb1IEUEAIQAw";
+    String KILL_3 = "CgkIldPb1IEUEAIQBA";
+    String KILL_5 = "CgkIldPb1IEUEAIQBQ";
 }
