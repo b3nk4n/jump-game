3f7eace1e523e5475183ecf74bdd8ba500687d8d
Benjamin Sautermeister <benjamin.sautermeister@instana.com>
2020-08-19 22:23:00 +0200

    Update maps and template to use single object layer with predefined types

--- android/assets/maps/OctoberBro.tsx
+++ android/assets/maps/OctoberBro.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.1" name="OctoberBro" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="64" columns="8">
- <image source="tileset.png" trans="ff00ff" width="137" height="137"/>
+<tileset version="1.4" tiledversion="1.4.1" name="OctoberBro" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="187" columns="11">
+ <image source="tileset.png" trans="ff00ff" width="188" height="290"/>
 </tileset>
--- core/src/de/bsautermeister/jump/physics/WorldCreator.java
+++ core/src/de/bsautermeister/jump/physics/WorldCreator.java
@@ -1,7 +1,6 @@
 package de.bsautermeister.jump.physics;
 
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.maps.Map;
 import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
@@ -47,34 +46,36 @@ public class WorldCreator {
     public static final String BG_TILES_KEY = "bgTiles";
     public static final String FG_TILES_KEY = "fgTiles";
 
-    private static final String GROUND_KEY = "ground";
-    private static final String BOXES_KEY = "boxes";
-    private static final String BRICKS_KEY = "bricks";
-    private static final String FOXES_KEY = "foxes";
-    private static final String HEDGEHOGS_KEY = "hedgehogs";
-    private static final String DRUNKEN_GUYS_KEY = "drunkenGuys";
-    private static final String FISHES_KEY = "fishes";
-    private static final String FROGS_KEY = "frogs";
-    private static final String RAVENS_KEY = "ravens";
-    private static final String COLLIDER_KEY = "collider";
-    private static final String WATER_KEY = "water";
-    private static final String GOAL_KEY = "goal";
-    private static final String START_KEY = "start";
-    private static final String BOUNCERS_KEY = "bouncers";
-    private static final String PLATFORMS_KEY = "platforms";
-    private static final String COINS_KEY = "coins";
-    private static final String SPIKES_KEY = "spikes";
-    private static final String POLES_KEY = "poles";
-    private static final String SNORER_KEY = "snorer";
-    private static final String ENEMY_SIGNAL_TRIGGERS_KEY = "enemySignalTriggers";
-    private static final String INFOS_KEY = "infos";
+    private static final String GROUND_TYPE = "ground";
+    private static final String BOX_TYPE = "box";
+    private static final String BRICK_TYPE = "brick";
+    private static final String FOX_TYPE = "fox";
+    private static final String HEDGEHOG_TYPE = "hedgehog";
+    private static final String DRUNKEN_GUY_TYPE = "drunkenGuy";
+    private static final String FISH_TYPE = "fish";
+    private static final String FROG_TYPE = "frog";
+    private static final String RAVEN_TYPE = "raven";
+    private static final String COLLIDER_TYPE = "collider";
+    private static final String WATER_TYPE = "water";
+    private static final String GOAL_TYPE = "goal";
+    private static final String START_TYPE = "start";
+    private static final String BOUNCER_TYPE = "bouncer";
+    private static final String PLATFORM_TYPE = "platform";
+    private static final String COIN_TYPE = "coin";
+    private static final String SPIKE_TYPE = "spike";
+    private static final String POLE_TYPE = "pole";
+    private static final String SNORER_TYPE = "snorer";
+    private static final String ENEMY_SIGNAL_TRIGGER_TYPE = "enemySignalTrigger";
+    private static final String INFO_TYPE = "info";
+
+    private static final String OBJECTS_LAYER = "objects";
 
     private final World world;
     private final TiledMap map;
     private TextureAtlas atlas;
     private GameCallbacks callbacks;
 
-    private Array<InteractiveTileObject> tileObjects = new Array<InteractiveTileObject>();
+    private Array<InteractiveTileObject> tileObjects = new Array<>();
 
     public WorldCreator(GameCallbacks callbacks, World world, TiledMap map, TextureAtlas atlas) {
         this.callbacks = callbacks;
@@ -84,30 +85,24 @@ public class WorldCreator {
     }
 
     public void buildFromMap() {
-        buildStaticLayer(GROUND_KEY, Bits.GROUND, false);
-        buildStaticLayer(COLLIDER_KEY, Bits.COLLIDER, true);
+        buildStaticObjects(GROUND_TYPE, Bits.GROUND, false);
+        buildStaticObjects(COLLIDER_TYPE, Bits.COLLIDER, true);
 
-        if (hasLayer(map, BRICKS_KEY)) {
-            for (MapObject mapObject : map.getLayers().get(BRICKS_KEY).getObjects().getByType(RectangleMapObject.class)) {
+        for (RectangleMapObject mapObject : getRectObjects(map)) {
+            if (isType(mapObject, BRICK_TYPE)) {
                 tileObjects.add(new Brick(callbacks, world, map, atlas, mapObject));
-            }
-        }
-
-        if (hasLayer(map, BOXES_KEY)) {
-            for (MapObject mapObject : map.getLayers().get(BOXES_KEY).getObjects().getByType(RectangleMapObject.class)) {
+            } else if (isType(mapObject, BOX_TYPE)) {
                 tileObjects.add(new ItemBox(callbacks, world, map, atlas, mapObject));
             }
         }
     }
 
-    private void buildStaticLayer(String layer, short categoryBit, boolean asSensor) {
-        if (!hasLayer(map, layer)) {
-            return;
-        }
-
-        for (MapObject mapObject : map.getLayers().get(layer).getObjects().getByType(RectangleMapObject.class)) {
-            Rectangle bounds = ((RectangleMapObject) mapObject).getRectangle();
-            createBody(null, world, bounds, BodyDef.BodyType.StaticBody, categoryBit, asSensor);
+    private void buildStaticObjects(String type, short categoryBit, boolean asSensor) {
+        for (RectangleMapObject mapObject : getRectObjects(map)) {
+            if (isType(mapObject, type)) {
+                createBody(null, world, mapObject.getRectangle(), BodyDef.BodyType.StaticBody,
+                        categoryBit, asSensor);
+            }
         }
     }
 
@@ -148,41 +143,33 @@ public class WorldCreator {
 
     public Array<Enemy> createEnemies() {
         Array<Enemy> enemies = new Array<>();
-        if (hasLayer(map, FOXES_KEY)) {
-            for (MapObject mapObject : map.getLayers().get(FOXES_KEY).getObjects().getByType(RectangleMapObject.class)) {
-                Rectangle rect = ((RectangleMapObject) mapObject).getRectangle();
+
+        for (RectangleMapObject mapObject : getRectObjects(map)) {
+            if (isType(mapObject, FOX_TYPE)) {
+                Rectangle rect = mapObject.getRectangle();
                 String group = (String) mapObject.getProperties().get("group");
                 boolean rightDirection = mapObject.getProperties().get("rightDirection", false, Boolean.class);
                 Fox fox = new Fox(callbacks, world, atlas,
                         rect.getX() / Cfg.PPM, rect.getY() / Cfg.PPM, rightDirection);
                 fox.setGroup(group);
                 enemies.add(fox);
-            }
-        }
-        if (hasLayer(map, HEDGEHOGS_KEY)) {
-            for (MapObject mapObject : map.getLayers().get(HEDGEHOGS_KEY).getObjects().getByType(RectangleMapObject.class)) {
-                Rectangle rect = ((RectangleMapObject) mapObject).getRectangle();
+            } else if (isType(mapObject, HEDGEHOG_TYPE)) {
+                Rectangle rect = mapObject.getRectangle();
                 String group = (String) mapObject.getProperties().get("group");
                 boolean rightDirection = mapObject.getProperties().get("rightDirection", false, Boolean.class);
                 Hedgehog hedgehog = new Hedgehog(callbacks, world, atlas,
                         rect.getX() / Cfg.PPM, rect.getY() / Cfg.PPM, rightDirection);
                 hedgehog.setGroup(group);
                 enemies.add(hedgehog);
-            }
-        }
-        if (hasLayer(map, DRUNKEN_GUYS_KEY)) {
-            for (MapObject mapObject : map.getLayers().get(DRUNKEN_GUYS_KEY).getObjects().getByType(RectangleMapObject.class)) {
-                Rectangle rect = ((RectangleMapObject) mapObject).getRectangle();
+            } else if (isType(mapObject, DRUNKEN_GUY_TYPE)) {
+                Rectangle rect = mapObject.getRectangle();
                 String group = (String) mapObject.getProperties().get("group");
                 DrunkenGuy drunkenGuy = new DrunkenGuy(callbacks, world, atlas,
                         rect.getX() / Cfg.PPM, rect.getY() / Cfg.PPM);
                 drunkenGuy.setGroup(group);
                 enemies.add(drunkenGuy);
-            }
-        }
-        if (hasLayer(map, FISHES_KEY)) {
-            for (MapObject mapObject : map.getLayers().get(FISHES_KEY).getObjects().getByType(RectangleMapObject.class)) {
-                Rectangle rect = ((RectangleMapObject) mapObject).getRectangle();
+            } else if (isType(mapObject, FISH_TYPE)) {
+                Rectangle rect = mapObject.getRectangle();
                 Float startDelay = (Float) mapObject.getProperties().get("startDelay");
                 Integer startAngle = (Integer) mapObject.getProperties().get("startAngle");
                 Float velocityFactor = (Float) mapObject.getProperties().get("velocityFactor");
@@ -194,22 +181,16 @@ public class WorldCreator {
                 fish.setVelocityFactor(velocityFactor != null ? velocityFactor : 1f);
                 fish.setGroup(group);
                 enemies.add(fish);
-            }
-        }
-        if (hasLayer(map, FROGS_KEY)) {
-            for (MapObject mapObject : map.getLayers().get(FROGS_KEY).getObjects().getByType(RectangleMapObject.class)) {
-                Rectangle rect = ((RectangleMapObject) mapObject).getRectangle();
+            } else if (isType(mapObject, FROG_TYPE)) {
+                Rectangle rect = mapObject.getRectangle();
                 String group = (String) mapObject.getProperties().get("group");
                 boolean rightDirection = mapObject.getProperties().get("rightDirection", false, Boolean.class);
                 Frog frog = new Frog(callbacks, world, atlas,
                         rect.getX() / Cfg.PPM, rect.getY() / Cfg.PPM, rightDirection);
                 frog.setGroup(group);
                 enemies.add(frog);
-            }
-        }
-        if (hasLayer(map, RAVENS_KEY)) {
-            for (MapObject mapObject : map.getLayers().get(RAVENS_KEY).getObjects().getByType(RectangleMapObject.class)) {
-                Rectangle rect = ((RectangleMapObject) mapObject).getRectangle();
+            } else if (isType(mapObject, RAVEN_TYPE)) {
+                Rectangle rect = mapObject.getRectangle();
                 String group = (String) mapObject.getProperties().get("group");
                 boolean rightDirection = mapObject.getProperties().get("rightDirection", false, Boolean.class);
                 boolean swinging = mapObject.getProperties().get("swinging", false, Boolean.class);
@@ -219,6 +200,7 @@ public class WorldCreator {
                 enemies.add(raven);
             }
         }
+
         return enemies;
     }
 
@@ -226,8 +208,8 @@ public class WorldCreator {
         Array<PlatformBouncer> bouncerRegions = getPlatformBouncerRegions();
         Array<Platform> platforms = new Array<>();
 
-        if (hasLayer(map, PLATFORMS_KEY)) {
-            for (RectangleMapObject mapObject : map.getLayers().get(PLATFORMS_KEY).getObjects().getByType(RectangleMapObject.class)) {
+        for (RectangleMapObject mapObject : getRectObjects(map)) {
+            if (isType(mapObject, PLATFORM_TYPE)) {
                 Rectangle rect = mapObject.getRectangle();
                 Integer startAngle = (Integer) mapObject.getProperties().get("startAngle");
                 Boolean breakable = (Boolean) mapObject.getProperties().get("breakable");
@@ -244,20 +226,21 @@ public class WorldCreator {
 
     public Array<Coin> createCoins() {
         Array<Coin> coins = new Array<>();
-        if (hasLayer(map, COINS_KEY)) {
-            for (RectangleMapObject mapObject : map.getLayers().get(COINS_KEY).getObjects().getByType(RectangleMapObject.class)) {
+        for (RectangleMapObject mapObject : getRectObjects(map)) {
+            if (isType(mapObject, COIN_TYPE)) {
                 Rectangle rect = mapObject.getRectangle();
                 coins.add(new Coin(callbacks, world, atlas,
                         rect.getX() / Cfg.PPM, rect.getY() / Cfg.PPM));
             }
+
         }
         return coins;
     }
 
     public Array<Rectangle> getWaterRegions() {
         Array<Rectangle> waterRegions = new Array<>();
-        if (hasLayer(map, WATER_KEY)) {
-            for (RectangleMapObject mapObject : map.getLayers().get(WATER_KEY).getObjects().getByType(RectangleMapObject.class)) {
+        for (RectangleMapObject mapObject : getRectObjects(map)) {
+            if (isType(mapObject, WATER_TYPE)) {
                 waterRegions.add(toPPM(mapObject.getRectangle()));
             }
         }
@@ -266,27 +249,19 @@ public class WorldCreator {
 
     public Array<Rectangle> getPoleRegions() {
         Array<Rectangle> waterRegions = new Array<>();
-        if (hasLayer(map, POLES_KEY)) {
-            for (RectangleMapObject mapObject : map.getLayers().get(POLES_KEY).getObjects().getByType(RectangleMapObject.class)) {
+        for (RectangleMapObject mapObject : getRectObjects(map)) {
+            if (isType(mapObject, POLE_TYPE)) {
                 waterRegions.add(toPPM(mapObject.getRectangle()));
             }
         }
         return waterRegions;
     }
 
-    public Rectangle getSnorerRegion() {
-        if (hasLayer(map, SNORER_KEY)) {
-            RectangleMapObject mapObject = map.getLayers().get(SNORER_KEY).getObjects().getByType(RectangleMapObject.class).first();
-            return toPPM(mapObject.getRectangle());
-        }
-        return null;
-    }
-
     public Array<Rectangle> getSpikeRegions() {
         Array<Rectangle> spikeRegions = new Array<>();
-        if (hasLayer(map, SPIKES_KEY)) {
-            for (RectangleMapObject mapObject : map.getLayers().get(SPIKES_KEY).getObjects().getByType(RectangleMapObject.class)) {
-                Rectangle rect = toPPM(mapObject.getRectangle());;
+        for (RectangleMapObject mapObject : getRectObjects(map)) {
+            if (isType(mapObject, SPIKE_TYPE)) {
+                Rectangle rect = toPPM(mapObject.getRectangle());
                 rect.setHeight(rect.getHeight() / 2);
                 spikeRegions.add(rect);
             }
@@ -294,19 +269,13 @@ public class WorldCreator {
         return spikeRegions;
     }
 
-    private static Rectangle toPPM(Rectangle rect) {
-        return new Rectangle(rect.x / Cfg.PPM, rect.y / Cfg.PPM,
-                rect.width / Cfg.PPM, rect.height / Cfg.PPM);
-    }
-
     private Array<PlatformBouncer> getPlatformBouncerRegions() {
         Array<PlatformBouncer> bouncerRegions = new Array<>();
-        if (hasLayer(map, BOUNCERS_KEY)) {
-            for (MapObject mapObject : map.getLayers().get(BOUNCERS_KEY).getObjects().getByType(RectangleMapObject.class)) {
-                Rectangle rect = ((RectangleMapObject) mapObject).getRectangle();
+        for (RectangleMapObject mapObject : getRectObjects(map)) {
+            if (isType(mapObject, BOUNCER_TYPE)) {
                 Integer angle = (Integer) mapObject.getProperties().get("bounceAngle");
                 PlatformBouncer platformBouncer = new PlatformBouncer(
-                        toPPM(rect), angle != null ? angle : 0
+                        toPPM(mapObject.getRectangle()), angle != null ? angle : 0
                 );
                 bouncerRegions.add(platformBouncer);
             }
@@ -318,32 +287,24 @@ public class WorldCreator {
         return tileObjects;
     }
 
+    public Rectangle getSnorerRegion() {
+        return toPPM(getFirstRectObject(map, SNORER_TYPE).getRectangle());
+    }
+
     public Rectangle getGoal() {
-        Rectangle rect = map.getLayers()
-                .get(GOAL_KEY)
-                .getObjects()
-                .getByType(RectangleMapObject.class)
-                .first()
-                .getRectangle();
-        return toPPM(rect);
+        return toPPM(getFirstRectObject(map, GOAL_TYPE).getRectangle());
     }
 
     public StartParams getStart() {
-        RectangleMapObject rect = map.getLayers()
-                .get(START_KEY)
-                .getObjects()
-                .getByType(RectangleMapObject.class)
-                .first();
-        return new StartParams(rect);
+        return new StartParams(getFirstRectObject(map, START_TYPE));
     }
 
     public Array<EnemySignalTrigger> getEnemySignalTriggers() {
         Array<EnemySignalTrigger> signalTriggers = new Array<>();
-        if (hasLayer(map, ENEMY_SIGNAL_TRIGGERS_KEY)) {
-            for (MapObject mapObject : map.getLayers().get(ENEMY_SIGNAL_TRIGGERS_KEY).getObjects().getByType(RectangleMapObject.class)) {
-                Rectangle rect = ((RectangleMapObject) mapObject).getRectangle();
+        for (RectangleMapObject mapObject : getRectObjects(map)) {
+            if (isType(mapObject, ENEMY_SIGNAL_TRIGGER_TYPE)) {
                 String group = (String) mapObject.getProperties().get("group");
-                signalTriggers.add(new EnemySignalTrigger(toPPM(rect), group));
+                signalTriggers.add(new EnemySignalTrigger(toPPM(mapObject.getRectangle()), group));
             }
         }
         return signalTriggers;
@@ -351,25 +312,20 @@ public class WorldCreator {
 
     public Array<InfoSign> getInfoSigns() {
         Array<InfoSign> infos = new Array<>();
-        if (hasLayer(map, INFOS_KEY)) {
-            for (MapObject mapObject : map.getLayers().get(INFOS_KEY).getObjects().getByType(RectangleMapObject.class)) {
-                Rectangle rect = ((RectangleMapObject) mapObject).getRectangle();
+        for (RectangleMapObject mapObject : getRectObjects(map)) {
+            if (isType(mapObject, INFO_TYPE)) {
                 String languageKey = (String) mapObject.getProperties().get("languageKey");
-                infos.add(new InfoSign(toPPM(rect), languageKey));
+                infos.add(new InfoSign(toPPM(mapObject.getRectangle()), languageKey));
             }
         }
         return infos;
-    }
-
-    private boolean hasLayer(Map map, String layer) {
-        return map.getLayers().get(layer) != null;
     }
 
     public static class StartParams {
         public final Vector2 centerPosition;
         public final boolean leftDirection;
 
-        public StartParams(RectangleMapObject mapObject) {
+        StartParams(RectangleMapObject mapObject) {
             Rectangle rect = mapObject.getRectangle();
             centerPosition = new Vector2((rect.x + rect.width / 2) / Cfg.PPM, (rect.y + rect.height / 2) / Cfg.PPM);
             leftDirection = mapObject.getProperties().get("leftDirection", false, Boolean.class);
@@ -380,7 +336,7 @@ public class WorldCreator {
         public final Rectangle rect;
         public final String group;
 
-        public EnemySignalTrigger(Rectangle rect, String group) {
+        EnemySignalTrigger(Rectangle rect, String group) {
             this.rect = rect;
             this.group = group;
         }
@@ -390,9 +346,31 @@ public class WorldCreator {
         public final Rectangle rect;
         public final String languageKey;
 
-        public InfoSign(Rectangle rect, String languageKey) {
+        InfoSign(Rectangle rect, String languageKey) {
             this.rect = rect;
             this.languageKey = languageKey;
         }
+    }
+
+    private static boolean isType(MapObject mapObject, String type) {
+        return mapObject.getProperties().get("type").equals(type);
+    }
+
+    private static Array<RectangleMapObject> getRectObjects(TiledMap map) {
+        return map.getLayers().get(OBJECTS_LAYER).getObjects().getByType(RectangleMapObject.class);
+    }
+
+    private static RectangleMapObject getFirstRectObject(TiledMap map, String type) {
+        for (RectangleMapObject mapObject : getRectObjects(map)) {
+            if (isType(mapObject, type)) {
+                return mapObject;
+            }
+        }
+        return null;
+    }
+
+    private static Rectangle toPPM(Rectangle rect) {
+        return new Rectangle(rect.x / Cfg.PPM, rect.y / Cfg.PPM,
+                rect.width / Cfg.PPM, rect.height / Cfg.PPM);
     }
 }
