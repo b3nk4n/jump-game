cf28292dafc8baa842dd1c1249a5f7d9ea2acb95
Benjamin Sautermeister <benjamin.sautermeister@instana.com>
2020-08-26 23:36:49 +0200

    Add enemies and objects to level 4

--- core/src/de/bsautermeister/jump/screens/game/level/LevelMetadata.java
+++ core/src/de/bsautermeister/jump/screens/game/level/LevelMetadata.java
@@ -7,7 +7,7 @@ public class LevelMetadata {
             new LevelInfo(150, 2000, 4000, 5000, 0),
             new LevelInfo(240, 6000, 7500, 9000, 1),
             new LevelInfo(240, 5000, 7500, 9000, 3),
-            new LevelInfo(99, 0, 999, 9999, 6),
+            new LevelInfo(210, 0, 999, 9999, 6),
             new LevelInfo(99, 0, 999, 9999, 9)
     };
 
