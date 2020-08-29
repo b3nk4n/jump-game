b0a482143e24772ba864f2ba2e6b9b59579c341d
Benjamin Sautermeister <benjamin.sautermeister@instana.com>
2020-08-29 14:58:22 +0200

    Update and extend tile-set to fix visual glitches

--- android/assets/maps/OctoberBro.tsx
+++ android/assets/maps/OctoberBro.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.1" name="OctoberBro" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="187" columns="11">
- <image source="tileset.png" trans="ff00ff" width="188" height="290"/>
+<tileset version="1.4" tiledversion="1.4.1" name="OctoberBro" tilewidth="16" tileheight="16" spacing="3" margin="2" tilecount="187" columns="11">
+ <image source="tileset.png" trans="ff00ff" width="210" height="324"/>
 </tileset>
