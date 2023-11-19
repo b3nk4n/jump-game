fe7123cf63715d0761f2235cd245b893560b7922
Benjamin Kan <Kan@ibm.com>
2023-11-19 15:18:56 -0800

    Add iOS project and allow to run in simulator
    yet with wrong aspect ratio and glitches (missing background, trasparency, ...)

---
+++ ios/src/de/bsautermeister/jump/IOSLauncher.java
@@ -0,0 +1,28 @@
+package de.bsautermeister.jump;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+
+import de.bsautermeister.jump.services.NoopGameServices;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new JumpGame(new NoopGameServices(), new GameEnv() {
+            @Override
+            public String getVersion() {
+                return "iOS version";
+            }
+        }), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
