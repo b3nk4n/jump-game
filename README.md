3c85aa026c3c826a77326525f1616f6c6a3190e3
Benjamin Kan <Kan@ibm.com>
2023-11-19 11:48:46 -0800

    Fix running Desktop project on MacOS

--- README.md
+++ README.md
@@ -35,6 +35,22 @@ Thanks to Dee Yan-Key for allowing me to use [his songs](https://freemusicarchiv
 
 Use Java 11 to build and run the project.
 
+### Troubleshooting
+
+### Desktop run configuration
+
+On MacOS, the VM argument `-XstartOnFirstThread` is required to launch the project on desktop.
+Setting this flag is already defined the in the `desktop:run` Gradle task. However, if you simply
+run the main method of the `DesktopLaumcher` class, the auto-created IntelliJ run configuration does
+not actually use that Gradle task. Instead, simply create this run configuration yourself:
+
+1. Select _Edit configurations..._
+2. Add a new _Gradle_ configuration
+3. Use `desktop:run` as the command to _Run_
+
+While this might only be strictly necessary for MacOS, it does not harm to do that for any platform,
+to ensure the proper Gradle task to run the desktop project is used.
+
 ## License
 
 This work is published under [MIT][mit] License.
--- desktop/src/de/bsautermeister/jump/desktop/DesktopLauncher.java
+++ desktop/src/de/bsautermeister/jump/desktop/DesktopLauncher.java
@@ -1,18 +1,19 @@
 package de.bsautermeister.jump.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
 import de.bsautermeister.jump.Cfg;
 import de.bsautermeister.jump.JumpGame;
 import de.bsautermeister.jump.services.NoopGameServices;
 
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "October Bro";
-		config.width = Cfg.WINDOW_WIDTH;
-		config.height = Cfg.WINDOW_HEIGHT;
-		new LwjglApplication(new JumpGame(new NoopGameServices()), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("October Bro");
+		config.setWindowedMode(Cfg.WINDOW_WIDTH, Cfg.WINDOW_HEIGHT);
+		new Lwjgl3Application(new JumpGame(new NoopGameServices()), config);
 	}
 }
